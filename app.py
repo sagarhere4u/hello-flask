7f878b1f3bbac0ee30900ee1794e0974dab36132
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-04-18 15:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Welcome to Jenkins Pipelines - Testing!!</h1>"
+    return "<h1 style='color:blue'>Welcome to Jenkins Pipelines - On Minion!!</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
