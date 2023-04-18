a26454822b5a7d0eb2dc289dde875d3f373690a0
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2023-04-18 10:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Good Morning!!</h1>"
+    return "<h1 style='color:blue'>Welcome, helloTest should RUN!!</h1>"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
